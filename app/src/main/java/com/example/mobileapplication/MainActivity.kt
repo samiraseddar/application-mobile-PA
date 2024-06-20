54a6b580dd62c82e4c6641a1e2d6701ca4656202
samiraseddar <seddar20191@gmail.com>
2024-06-20 14:35:45 +0200

    initialisation du projet avec Jetpack Compose

---
+++ app/src/main/java/com/example/mobileapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp()
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    Column {
+        Text(text = "Hello, Jetpack Compose!")
+    }
+}
