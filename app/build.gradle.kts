e81e2ff7701184fa39b243a28ac177373f914bb4
samiraseddar <seddar20191@gmail.com>
2024-06-20 14:49:01 +0200

    Configurer les Dépendances de Jetpack Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,10 @@ android {
 }
 
 dependencies {
+    implementation ("androidx.core:core-ktx:1.9.0")
+    implementation ("androidx.activity:activity-compose:1.5.1")
+    implementation ("androidx.compose.ui:ui:1.2.1")
+    implementation ("androidx.compose.material3:material3:1.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
