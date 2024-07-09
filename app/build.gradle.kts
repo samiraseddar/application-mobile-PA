838963029d5aaea0d58ac266b70375708f6ae183
samiraseddar <seddar20191@gmail.com>
2024-07-09 11:56:07 +0200

    add dependence pour configurer l'api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,9 @@ android {
 }
 
 dependencies {
+    implementation ("com.google.code.gson:gson:2.8.8")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0'")
     implementation ("androidx.compose.ui:ui:1.0.5")
     implementation ("androidx.compose.material:material:1.0.5")
     implementation ("androidx.activity:activity-compose:1.4.0")
